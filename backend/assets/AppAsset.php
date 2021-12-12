549e3c6c793d8f7cce7fc96bcb223c68f24c376a
Ryan Hidayattulloh <ryanhidayattulloh76@gmail.com>
2021-12-12 13:45:55 -0500

    Bismillah

---
+++ backend/assets/AppAsset.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main backend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'vendor/fontawesome-free/css/all.min.css',
+        'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i',
+        'css/sb-admin-2.min.css',
+    ];
+    public $js = [
+        "vendor/jquery/jquery.min.js",
+        "vendor/bootstrap/js/bootstrap.bundle.min.js",
+        "vendor/jquery-easing/jquery.easing.min.js",
+        "js/sb-admin-2.min.js",
+
+
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap4\BootstrapAsset',
+    ];
+}
