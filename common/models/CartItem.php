61791af2aecd540196bdf9573ad8323f14ff8dbd
Ryan Hidayattulloh <ryanhidayattulloh76@gmail.com>
2021-12-22 21:11:06 -0800

    fix

--- common/models/CartItem.php
+++ common/models/CartItem.php
@@ -11,7 +11,9 @@ use Yii;
  * @property int $user_id
  * @property int $barang_id
  * @property int $jumlah
- * @property int $created_by
+ *
+ * @property Barang $barang
+ * @property User $user
  */
 class CartItem extends \yii\db\ActiveRecord
 {
@@ -29,8 +31,10 @@ class CartItem extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['user_id', 'barang_id', 'jumlah', 'created_by'], 'required'],
-            [['user_id', 'barang_id', 'jumlah', 'created_by'], 'integer'],
+            [['user_id', 'barang_id', 'jumlah'], 'required'],
+            [['user_id', 'barang_id', 'jumlah'], 'integer'],
+            [['barang_id'], 'exist', 'skipOnError' => true, 'targetClass' => Barang::className(), 'targetAttribute' => ['barang_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
         ];
     }
 
@@ -44,16 +48,35 @@ class CartItem extends \yii\db\ActiveRecord
             'user_id' => 'User ID',
             'barang_id' => 'Barang ID',
             'jumlah' => 'Jumlah',
-            'created_by' => 'Created By',
         ];
     }
 
     /**
+     * Gets query for [[Barang]].
+     *
+     * @return \yii\db\ActiveQuery|BarangQuery
+     */
+    public function getBarang()
+    {
+        return $this->hasOne(Barang::className(), ['id' => 'barang_id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
      * {@inheritdoc}
-     * @return \common\models\query\CartItemQuery the active query used by this AR class.
+     * @return CartItemQuery the active query used by this AR class.
      */
     public static function find()
     {
-        return new \common\models\query\CartItemQuery(get_called_class());
+        return new CartItemQuery(get_called_class());
     }
 }
