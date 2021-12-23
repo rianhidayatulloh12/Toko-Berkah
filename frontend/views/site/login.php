e1646ac15645ce359be087d738d30dabb23c3bad
Ryan Hidayattulloh <ryanhidayattulloh76@gmail.com>
2021-12-22 21:08:43 -0800

    modifikasi login

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -11,12 +11,13 @@ $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <div class="row justify-content-center">
 
-    <div class="row">
         <div class="col-lg-5">
+            <h1><?= Html::encode($this->title) ?></h1>
+
+            <p>Please fill out the following fields to login:</p>
             <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
